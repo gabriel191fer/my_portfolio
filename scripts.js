79b2b46d4015dfb1f7924f5884c41c7050bbe8d0
Gabriel da Paixao Fernandes <gabriel.fernandes@basa.com.br>
2023-03-14 11:38:02 -0300

    update

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ const nav = document.getElementById('navbar');
 window.addEventListener("scroll", function (event) {
   if (window.pageYOffset > 100) {
 
-    nav.style.background = "#8c15e9";
-    nav.style.color = "black"
+    nav.style.background = "#172d67";
+    nav.style.color = "#22ddd2"
 
   }
   else {
