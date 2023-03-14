b822e9f428fdeb5ecf2076e862cb46ef8d7e3862
Gabriel da Paixao Fernandes <gabriel.fernandes@basa.com.br>
2023-03-14 16:59:47 -0300

    update 3

--- scripts.js
+++ scripts.js
@@ -3,12 +3,23 @@ window.addEventListener("scroll", function (event) {
   if (window.pageYOffset > 100) {
 
     nav.style.background = "#172d67";
-    nav.style.color = "#22ddd2"
+    nav.style.color = "#22ddd2";
+  
 
   }
   else {
     nav.style.background = "transparent";
-    nav.style.color = "#8c15e9"
+    nav.style.textDecoration = "none";
+    nav.style.color = "#22ddd2";
 
   }
 });
+
+function burger() {
+  var x = document.getElementById("nav_div");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
