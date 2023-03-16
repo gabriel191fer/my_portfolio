87afa7c1e0f3ccbd868ceb35de96b8c64d37a689
Gabriel da Paixao Fernandes <gabriel.fernandes@basa.com.br>
2023-03-16 15:27:36 -0300

    navbar responsitivy added

--- scripts.js
+++ scripts.js
@@ -1,40 +1,38 @@
 const nav = document.getElementById('navbar');
-const bg_nav = document.getElementById('nav_div');
+const bg_nav = document.getElementById('nav_div_mobile');
 window.addEventListener("scroll", function (event) {
   if (window.pageYOffset > 100) {
-
     nav.style.background = "#172d67";
     bg_nav.style.display = "none";
     bg_nav.style.background = "#172d67"
     nav.style.color = "#22ddd2";
-
-
   }
   else {
     nav.style.background = "transparent";
     bg_nav.style.background = "transparent"
     nav.style.textDecoration = "none";
     nav.style.color = "#22ddd2";
-
   }
 });
 
 
 
 function burger() {
-  var x = document.getElementById("nav_div");
-  if (x.style.animationName === "slidein") {
-    x.style.animationName = "slideout";
-
-   
-
+  var x = document.getElementById("nav_div_mobile");
+  if (x.style.display === "none") {
+    x.style.animationName = "slidein";
+    setTimeout(() => {
+      x.style.display = "block", 200;
+      nav.style.background = "#172d67";
+      bg_nav.style.background = "#172d67"
+    } )
   } else {
-    x.style.display = "block";
-    x.style.animationName = "slidein"
+    x.style.animationName = "slideout"
+    setTimeout(() => { 
+      x.style.display = "none", 200;
+      nav.style.background = "transparent";
+      bg_nav.style.background = "transparent"
+    })
   }
 }
 
-
-function codingCourse() {
-  console.log("freeCodeCamp");
-}
